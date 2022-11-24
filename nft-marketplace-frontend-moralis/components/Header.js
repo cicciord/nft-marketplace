33a54706011502ec2c1cbe9715cc55f39c33155a
thirdeth <thirdeth@users.noreply.github.com>
2022-11-24 23:48:26 +0100

    header style

--- nft-marketplace-frontend-moralis/components/Header.js
+++ nft-marketplace-frontend-moralis/components/Header.js
@@ -3,10 +3,17 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <nav>
-            <Link href="/">Marketplace</Link>
-            <Link href="/sell-nft">Sell</Link>
-            <ConnectButton />
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-3xl"> NFT Marketplace</h1>
+            <div className="flex flex-row items-center">
+                <Link href="/" className="mr-4 p-6">
+                    Home
+                </Link>
+                <Link href="/sell-nft" className="mr-4 p-6">
+                    Sell
+                </Link>
+                <ConnectButton moralisAuth={false} />
+            </div>
         </nav>
     )
 }
